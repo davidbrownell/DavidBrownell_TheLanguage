83fc7c259aabb2ff9f9cc8af0aa85fff23ca2d8d
David Brownell <db@DavidBrownell.com>
2021-06-27 16:34:27 -0700

    Added TokenStatement and tests

---
+++ src/TheLanguage/ParserImpl/StatementImpl/TokenStatement.py
@@ -0,0 +1,126 @@
+# ----------------------------------------------------------------------
+# |
+# |  TokenStatement.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2021-06-25 15:48:38
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2021
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Contains the TokenStatement object"""
+
+import os
+
+from typing import Optional, Tuple, Union
+
+from dataclasses import dataclass
+
+import CommonEnvironment
+from CommonEnvironment import Interface
+
+from CommonEnvironmentEx.Package import InitRelativeImports
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+with InitRelativeImports():
+    from .Statement import Statement
+
+    from ..Token import (
+        DedentToken,
+        IndentToken,
+        Token as TokenClass,
+    )
+
+
+# ----------------------------------------------------------------------
+class TokenStatement(Statement):
+    """Statement that wraps a Token"""
+
+    # ----------------------------------------------------------------------
+    def __init__(
+        self,
+        token: TokenClass,
+    ):
+        super(TokenStatement, self).__init__(token.Name)
+
+        self.Token                          = token
+
+    # ----------------------------------------------------------------------
+    @Interface.override
+    def Parse(
+        self,
+        normalized_iter: Statement.NormalizedIterator,
+        observer: Statement.Observer,
+        ignore_whitespace=False,
+        single_threaded=False,
+    ) -> Union[
+        Statement.ParseResult,
+        None,
+    ]:
+        # We only want to consume whitespace if there is a match that follows
+        potential_iter = normalized_iter.Clone()
+        potential_whitespace = self._ExtractWhitespace(potential_iter)
+        potential_iter_begin = potential_iter.Clone()
+
+        result = self.Token.Match(potential_iter)
+        if result is None:
+            return Statement.ParseResult(False, normalized_iter, None)
+
+        data = Statement.TokenParseResultData(
+            self.Token,
+            potential_whitespace,
+            result,
+            potential_iter_begin,
+            potential_iter,
+            IsIgnored=self.Token.IsAlwaysIgnored,
+        )
+
+        if isinstance(self.Token, IndentToken):
+            observer.OnIndent(data)
+        elif isinstance(self.Token, DedentToken):
+            observer.OnDedent(data)
+
+        return Statement.ParseResult(True, potential_iter, data)
+
+    # ----------------------------------------------------------------------
+    # |
+    # |  Private Methods
+    # |
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def _ExtractWhitespace(
+        normalized_iter: Statement.NormalizedIterator,
+    ) -> Optional[Tuple[int, int]]:
+        """Consumes any whitespace located at the current offset"""
+
+        if not normalized_iter.AtEnd():
+            if normalized_iter.Offset == normalized_iter.LineInfo.OffsetStart:
+                if (
+                    not normalized_iter.LineInfo.HasNewIndent()
+                    and not normalized_iter.LineInfo.HasNewDedents()
+                ):
+                    normalized_iter.SkipPrefix()
+
+            else:
+                start = normalized_iter.Offset
+
+                while (
+                    normalized_iter.Offset < normalized_iter.LineInfo.OffsetEnd
+                    and normalized_iter.Content[normalized_iter.Offset].isspace()
+                    and normalized_iter.Content[normalized_iter.Offset] != "\n"
+                ):
+                    normalized_iter.Advance(1)
+
+                if normalized_iter.Offset != start:
+                    return start, normalized_iter.Offset
+
+        return None
---
+++ src/TheLanguage/ParserImpl/StatementImpl/UnitTests/TokenStatement_UnitTest.py
@@ -0,0 +1,347 @@
+# ----------------------------------------------------------------------
+# |
+# |  TokenStatement_UnitTest.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2021-06-27 15:53:00
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2021
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Unit test for TokenStatement.py"""
+
+import os
+import re
+import textwrap
+
+from unittest.mock import Mock
+
+import pytest
+
+import CommonEnvironment
+
+from CommonEnvironmentEx.Package import InitRelativeImports
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+with InitRelativeImports():
+    from ..TokenStatement import *
+
+    from ...Normalize import Normalize
+    from ...NormalizedIterator import NormalizedIterator
+
+    from ...Token import (
+        DedentToken,
+        IndentToken,
+        NewlineToken,
+        RegexToken,
+    )
+
+
+# ----------------------------------------------------------------------
+@pytest.fixture
+def parse_mock():
+    return Mock()
+
+# ----------------------------------------------------------------------
+def CreateIterator(
+    content: str,
+):
+    return NormalizedIterator(Normalize(content))
+
+# ----------------------------------------------------------------------
+class TestWords(object):
+    _word_statement                         = TokenStatement(RegexToken("Word", re.compile(r"(?P<value>[a-zA-Z0-9]+)\b")))
+    _newline_statement                      = TokenStatement(NewlineToken())
+    _indent_statement                       = TokenStatement(IndentToken())
+    _dedent_statement                       = TokenStatement(DedentToken())
+
+    # ----------------------------------------------------------------------
+    def test_Match(self, parse_mock):
+        iter = CreateIterator("This      is\ta \t\t   test\t  \n")
+
+        # This
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            4
+                Word <<Regex: <_sre.SRE_Match object; span=(0, 4), match='This'>>> ws:None [1, 1 -> 1, 5]
+            """,
+        )
+
+        assert iter.Offset == 0
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # is
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            12
+                Word <<Regex: <_sre.SRE_Match object; span=(10, 12), match='is'>>> ws:(4, 10) [1, 11 -> 1, 13]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # a
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            14
+                Word <<Regex: <_sre.SRE_Match object; span=(13, 14), match='a'>>> ws:(12, 13) [1, 14 -> 1, 15]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # test
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            24
+                Word <<Regex: <_sre.SRE_Match object; span=(20, 24), match='test'>>> ws:(14, 20) [1, 21 -> 1, 25]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # Newline
+        result = self._newline_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            28
+                Newline+ <<27, 28>> ws:(24, 27) [1, 28 -> 2, 1]
+            """,
+        )
+
+        assert result.Iter.AtEnd()
+        assert len(parse_mock.method_calls) == 0
+
+    # ----------------------------------------------------------------------
+    def test_NotAMatch(self, parse_mock):
+        iter = CreateIterator("te__")
+
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            False
+            0
+                <No Data>
+            """,
+        )
+
+        assert iter.Offset == 0
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+    # ----------------------------------------------------------------------
+    def test_IndentSimple(self, parse_mock):
+        iter = CreateIterator(
+            textwrap.dedent(
+                """\
+                one
+                    two
+                """,
+            ),
+        )
+
+        # One
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            3
+                Word <<Regex: <_sre.SRE_Match object; span=(0, 3), match='one'>>> ws:None [1, 1 -> 1, 4]
+            """,
+        )
+
+        assert iter.Offset == 0
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # Newline
+        result = self._newline_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            4
+                Newline+ <<3, 4>> ws:None [1, 4 -> 2, 1]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 0
+
+        iter = result.Iter
+
+        # Indent
+        result = self._indent_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            8
+                Indent <<4, 8, (4)>> ws:None [2, 1 -> 2, 5]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 1
+        assert parse_mock.method_calls[0] == ("OnIndent", (result.Data,), {})
+
+        iter = result.Iter
+
+        # two
+        result = self._word_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            11
+                Word <<Regex: <_sre.SRE_Match object; span=(8, 11), match='two'>>> ws:None [2, 5 -> 2, 8]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 1
+
+        iter = result.Iter
+
+        # Newline
+        result = self._newline_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            12
+                Newline+ <<11, 12>> ws:None [2, 8 -> 3, 1]
+            """,
+        )
+
+        assert result.Iter.AtEnd() == False
+        assert len(parse_mock.method_calls) == 1
+
+        iter = result.Iter
+
+        # Dedent
+        result = self._dedent_statement.Parse(iter, parse_mock)
+        assert str(result) == textwrap.dedent(
+            """\
+            True
+            12
+                Dedent <<>> ws:None [3, 1 -> 3, 1]
+            """,
+        )
+
+        assert result.Iter.AtEnd()
+        assert len(parse_mock.method_calls) == 2
+        assert parse_mock.method_calls[1] == ("OnDedent", (result.Data,), {})
+
+    # ----------------------------------------------------------------------
+    def test_IndentMoreComplex(self, parse_mock):
+        iter = CreateIterator(
+            textwrap.dedent(
+                """\
+                one
+                    two
+                        three
+                        four
+                    five
+                            six
+                    seven
+                        eight
+                """,
+            ),
+        )
+
+        num_events = 0
+
+        for expected_statement, expected_text in [
+            # one
+            (self._word_statement, "one"),
+            (self._newline_statement, None),
+
+            # two
+            (self._indent_statement, None),
+            (self._word_statement, "two"),
+            (self._newline_statement, None),
+
+            # three
+            (self._indent_statement, None),
+            (self._word_statement, "three"),
+            (self._newline_statement, None),
+
+            # four
+            (self._word_statement, "four"),
+            (self._newline_statement, None),
+
+            # five
+            (self._dedent_statement, None),
+            (self._word_statement, "five"),
+            (self._newline_statement, None),
+
+            # six
+            (self._indent_statement, None),
+            (self._word_statement, "six"),
+            (self._newline_statement, None),
+
+            # seven
+            (self._dedent_statement, None),
+            (self._word_statement, "seven"),
+            (self._newline_statement, None),
+
+            # eight
+            (self._indent_statement, None),
+            (self._word_statement, "eight"),
+            (self._newline_statement, None),
+
+            # eof
+            (self._dedent_statement, None),
+            (self._dedent_statement, None),
+        ]:
+            result = expected_statement.Parse(iter, parse_mock)
+            assert result.Success
+
+            if expected_text is not None:
+                assert result.Data.Value.Match.group("value") == expected_text
+
+            if result == self._indent_statement:
+                event_name = "OnIndent"
+            elif result == self._dedent_statement:
+                event_name = "OnDedent"
+            else:
+                event_name = None
+
+            if event_name is not None:
+                num_events += 1
+
+                assert len(parse_mock.method_calls) == num_events
+                assert parse_mock.method_calls[-1] == (method_name, (result.Data,), {})
+
+            iter = result.Iter
+
+        assert iter.AtEnd()
