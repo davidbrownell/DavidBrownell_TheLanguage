01e53d04054f5770f11dffeb55b94a8098cea179
David Brownell <db@DavidBrownell.com>
2021-07-30 13:45:44 -0700

    Organized Parser content into its own directory (allow banned text)

---
+++ src/TheLanguage/Lexer/__init__.py
@@ -0,0 +1,56 @@
+# TODO
+
+# Expressions
+# -----------
+# [DONE] - Cast: CastExpression
+# [DONE] - Func Invocation: FuncInvocationNode
+# [DONE] - Chained func invocation: FuncInvocationNode
+# - Index Invocation
+# [DONE] - Tuple: TupleNode
+# [DONE] - Ternary: TernaryExpression
+# - Generator/Comprehension
+# [DONE] - lambda: FuncDefinitionNode
+# [DONE] - comparison (<, <=, ==, !=, >, >=): BinaryExpression
+# [DONE] - math (+, -, *, /, //, **, %): MathBinaryNode: BinaryExpression
+# [DONE] - binary logical (and, or, in?, is?): BinaryExpression
+# [DONE] - unary logical (not): UnaryExpression
+#
+#
+#
+# Statements
+# ----------
+# - Class/Interface/Mixin/Exception Declaration
+# - Method Declaration
+# [DONE] - Func Declaration: FuncDefinitionNode
+# [DONE] - Func Invocation: FuncInvocationNode
+# - assign (<math> and <bitwise>)
+# <not necessary> - Import
+# - Noop
+# [DONE] - Tuple Var Decl: TupleNode
+# - Var Decl
+# - try/catch/finally
+# - throw
+# - if/elif/else
+# - while
+# - for
+# - Scoped Ref
+# - Quick Ref
+# - Pattern Matching/super else
+# - type alias
+# - return
+# - yield
+# - yield from
+# - delete?
+# - break
+# - continue
+# - attributes
+# - decorator?
+# - docstring
+# <not necessary> - compiler extension block (probably not necessary)
+#
+# Types
+# -----
+# [DONE] - Standard: StandardType
+# [DONE] - Tuple: TupleNode
+# [DONE] - Variant: VariantType
+# [Done] - Function: FirstClassFunctionType
