1a8c8aeec79f5e504558c49448eed4dd29a3129d
David Brownell <db@DavidBrownell.com>
2021-09-08 16:50:36 -0700

    Added TupleTypeLexerInfo.py and using it with TupleType.py

---
+++ src/TheLanguage/Lexer/ParserInterfaces/Types/TupleTypeLexerInfo.py
@@ -0,0 +1,40 @@
+# ----------------------------------------------------------------------
+# |
+# |  TupleTypeLexerInfo.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2021-09-08 16:39:29
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2021
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Contains the TupleTypeLexerInfo object"""
+
+import os
+
+from typing import List
+
+from dataclasses import dataclass
+
+import CommonEnvironment
+
+from CommonEnvironmentEx.Package import InitRelativeImports
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+with InitRelativeImports():
+    from ...LexerInfo import LexerInfo
+
+
+# ----------------------------------------------------------------------
+@dataclass(frozen=True, repr=False)
+class TupleTypeLexerInfo(LexerInfo):
+    Types: List[LexerInfo]
