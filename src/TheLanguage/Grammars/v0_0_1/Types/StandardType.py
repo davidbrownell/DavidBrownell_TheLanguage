edaff0698474c123db8e61ca8aa29601e92e4426
David Brownell <db@DavidBrownell.com>
2021-09-08 16:38:55 -0700

    Added StandardTypeLexerInfo.py and using it with StandardType.py

--- src/TheLanguage/Grammars/v0_0_1/Types/StandardType.py
+++ src/TheLanguage/Grammars/v0_0_1/Types/StandardType.py
@@ -17,7 +17,7 @@
 
 import os
 
-from dataclasses import dataclass
+from typing import cast, Dict, Optional, Union
 
 import CommonEnvironment
 from CommonEnvironment import Interface
@@ -32,19 +32,20 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 with InitRelativeImports():
     from ..Common import Tokens as CommonTokens
     from ..Common import TypeModifier
-    from ...GrammarPhrase import GrammarPhrase, ValidationError
+
+    from ...GrammarPhrase import GrammarPhrase
+
+    from ....Lexer.ParserInterfaces.Types.StandardTypeLexerInfo import StandardTypeLexerInfo
+
     from ....Parser.Phrases.DSL import (
         CreatePhrase,
+        ExtractOptional,
+        ExtractSequence,
+        ExtractToken,
+        Leaf,
+        Node,
         PhraseItem,
     )
-
-
-# ----------------------------------------------------------------------
-@dataclass(frozen=True)
-class InvalidTypeError(ValidationError):
-    Name: str
-
-    MessageTemplate                         = Interface.DerivedProperty("'{Name}' is not a valid type name; names must start with an uppercase letter and be at least 2 characters.")  # type: ignore
 
 
 # ----------------------------------------------------------------------
@@ -78,5 +79,45 @@ class StandardType(GrammarPhrase):
                         arity="?",
                     ),
                 ],
+            ),
+        )
+
+    # ----------------------------------------------------------------------
+    @classmethod
+    @Interface.override
+    def ValidateSyntax(
+        cls,
+        node: Node,
+    ) -> Optional[GrammarPhrase.ValidateSyntaxResult]:
+        token_lookup: Dict[str, Union[Leaf, Node]] = {
+            "self": node,
+        }
+
+        nodes = ExtractSequence(node)
+        assert len(nodes) == 2
+
+        # <type_name>
+        type_leaf = cast(Leaf, nodes[0])
+        type_name = cast(str, ExtractToken(type_leaf))
+        token_lookup["TypeName"] = type_leaf
+
+        # <modifier>?
+        modifier_node = cast(Optional[Node], ExtractOptional(cast(Optional[Node], nodes[1])))
+
+        if modifier_node is not None:
+            modifier = TypeModifier.Extract(modifier_node)
+            token_lookup["Modifier"] = modifier_node
+        else:
+            modifier = None
+
+        # Set the data
+        object.__setattr__(
+            node,
+            "Info",
+            # pylint: disable=too-many-function-args
+            StandardTypeLexerInfo(
+                token_lookup,
+                type_name,
+                modifier,  # type: ignore
             ),
         )
--- src/TheLanguage/Lexer/ParserInterfaces/Types/StandardTypeLexerInfo.py
+++ src/TheLanguage/Lexer/ParserInterfaces/Types/StandardTypeLexerInfo.py
@@ -1,9 +1,9 @@
 # ----------------------------------------------------------------------
 # |
-# |  TupleNameLexerInfo.py
+# |  StandardTypeLexerInfo.py
 # |
 # |  David Brownell <db@DavidBrownell.com>
-# |      2021-09-08 14:42:59
+# |      2021-09-08 16:24:25
 # |
 # ----------------------------------------------------------------------
 # |
@@ -13,11 +13,11 @@
 # |  http://www.boost.org/LICENSE_1_0.txt.
 # |
 # ----------------------------------------------------------------------
-"""Contains the TupleNameLexerInfo object"""
+"""Contains the StandardTypeLexerInfo object"""
 
 import os
 
-from typing import List
+from typing import Optional
 
 from dataclasses import dataclass
 
@@ -31,12 +31,12 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 # ----------------------------------------------------------------------
 
 with InitRelativeImports():
+    from ..Common.TypeModifier import TypeModifier
     from ...LexerInfo import LexerInfo
 
 
 # ----------------------------------------------------------------------
 @dataclass(frozen=True, repr=False)
-class TupleNameLexerInfo(LexerInfo):
-    Names: List[LexerInfo]
-
-    # TODO: Check for unique names
+class StandardTypeLexerInfo(LexerInfo):
+    TypeName: str
+    Modifier: Optional[TypeModifier]
