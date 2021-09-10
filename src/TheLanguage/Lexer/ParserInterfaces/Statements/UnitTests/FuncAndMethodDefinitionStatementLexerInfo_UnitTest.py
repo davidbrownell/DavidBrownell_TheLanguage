71aa57c890813d0a96357ab109c88803a6773f13
David Brownell <db@DavidBrownell.com>
2021-09-10 10:53:03 -0700

    Added placeholder tests (allow banned text)

---
+++ src/TheLanguage/Lexer/ParserInterfaces/Statements/UnitTests/FuncAndMethodDefinitionStatementLexerInfo_UnitTest.py
@@ -0,0 +1,36 @@
+# ----------------------------------------------------------------------
+# |
+# |  FuncAndMethodDefinitionStatementLexerInfo_UnitTest.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2021-09-10 10:50:29
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2021
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Unit tests for FuncAndMethodDefinitionStatementLexerInfo.py"""
+
+import os
+
+import CommonEnvironment
+
+from CommonEnvironmentEx.Package import InitRelativeImports
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+with InitRelativeImports():
+    from ..FuncAndMethodDefinitionStatementLexerInfo import *
+
+
+# ----------------------------------------------------------------------
+# Placeholder test
+def test_BugBug():
+    assert True
