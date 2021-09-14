3b7b6fc317ef4473befb8c0160cce505f740227c
David Brownell <db@DavidBrownell.com>
2021-09-13 19:13:42 -0700

    Added UnaryExpressionLexerInfo.py and using it in UnaryExpression.py

--- src/TheLanguage/Grammars/v0_0_1/Expressions/UnaryExpression.py
+++ src/TheLanguage/Grammars/v0_0_1/Expressions/UnaryExpression.py
@@ -17,7 +17,10 @@
 
 import os
 
+from typing import cast, Optional
+
 import CommonEnvironment
+from CommonEnvironment import Interface
 
 from CommonEnvironmentEx.Package import InitRelativeImports
 
@@ -27,9 +30,29 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 # ----------------------------------------------------------------------
 
 with InitRelativeImports():
-    from ..Common import Tokens as CommonTokens
-    from ...GrammarPhrase import GrammarPhrase
-    from ....Parser.Phrases.DSL import CreatePhrase, DynamicPhrasesType, PhraseItem
+    from ...GrammarPhrase import CreateLexerRegions, GrammarPhrase
+
+    from ....Lexer.Expressions.UnaryExpressionLexerInfo import (
+        ExpressionLexerInfo,
+        OperatorType,
+        UnaryEpressionLexerData,
+        UnaryExpressionLexerRegions,
+        UnaryExpressionLexerInfo,
+    )
+
+    from ....Lexer.LexerInfo import GetLexerInfo, SetLexerInfo
+
+    from ....Parser.Phrases.DSL import (
+        CreatePhrase,
+        DynamicPhrasesType,
+        ExtractDynamic,
+        ExtractOr,
+        ExtractSequence,
+        ExtractToken,
+        Leaf,
+        Node,
+        PhraseItem,
+    )
 
 
 # ----------------------------------------------------------------------
@@ -85,3 +108,63 @@ class UnaryExpression(GrammarPhrase):
                 ],
             ),
         )
+
+    # ----------------------------------------------------------------------
+    @classmethod
+    @Interface.override
+    def ExtractLexerInfo(
+        cls,
+        node: Node,
+    ) -> Optional[GrammarPhrase.ExtractLexerInfoResult]:
+        # ----------------------------------------------------------------------
+        def CreateLexerInfo():
+            nodes = ExtractSequence(node)
+            assert len(nodes) == 2
+
+            # <op>
+            operator_leaf = cast(Leaf, ExtractOr(cast(Node, nodes[0])))
+            op_value = cast(
+                str,
+                ExtractToken(
+                    operator_leaf,
+                    use_match=True,
+                ),
+            )
+
+            if op_value == "await":
+                operator_data = OperatorType.Await
+            elif op_value == "copy":
+                operator_data = OperatorType.Copy
+            elif op_value == "move":
+                operator_data = OperatorType.Move
+            elif op_value == "not":
+                operator_data = OperatorType.Not
+            elif op_value == "+":
+                operator_data = OperatorType.Positive
+            elif op_value == "-":
+                operator_data = OperatorType.Negative
+            elif op_value == "~":
+                operator_data = OperatorType.BitCompliment
+            else:
+                assert False, op_value
+
+            # <expr>
+            expr_node = ExtractDynamic(cast(Node, nodes[1]))
+            expr_data = cast(ExpressionLexerInfo, GetLexerInfo(expr_node))
+
+            SetLexerInfo(
+                node,
+                UnaryExpressionLexerInfo(
+                    UnaryEpressionLexerData(operator_data, expr_data),
+                    CreateLexerRegions(
+                        UnaryExpressionLexerRegions,  # type: ignore
+                        node,
+                        operator_leaf,
+                        expr_node,
+                    ),
+                ),
+            )
+
+        # ----------------------------------------------------------------------
+
+        return GrammarPhrase.ExtractLexerInfoResult(CreateLexerInfo)
