e7ab137ea5c0fabfc189be5b0ae305403b497a0b
David Brownell <db@DavidBrownell.com>
2022-04-14 23:45:16 -0700

    Added constraint creation and validation functionality

---
+++ src/TheLanguage/v1/Parser/ConstraintExpressions/IsConstraintExpression.py
@@ -0,0 +1,77 @@
+# ----------------------------------------------------------------------
+# |
+# |  IsConstraintExpression.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2022-04-14 12:36:58
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2022
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Contains the IsConstraintException object"""
+
+import os
+
+from typing import Any, Dict
+
+from dataclasses import dataclass
+
+import CommonEnvironment
+from CommonEnvironment import Interface
+
+from CommonEnvironmentEx.Package import InitRelativeImports
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+with InitRelativeImports():
+    from .ConstraintExpressionPhrase import CompileTimeType, ConstraintExpressionPhrase
+    from ..Common.CompileTimeTypes.Boolean import Boolean
+
+
+# ----------------------------------------------------------------------
+@dataclass(frozen=True, repr=False)
+class IsConstraintExpression(ConstraintExpressionPhrase):
+    expression: ConstraintExpressionPhrase
+    check_type: CompileTimeType
+
+    # ----------------------------------------------------------------------
+    def __post_init__(self, regions):
+        super(IsConstraintExpression, self).__post_init__(regions)
+
+    # ----------------------------------------------------------------------
+    @Interface.override
+    def Eval(
+        self,
+        args: Dict[str, Any],
+        type_overloads: Dict[str, CompileTimeType],
+    ) -> ConstraintExpressionPhrase.EvalResult:
+        result = self.expression.Eval(args, type_overloads)
+
+        type_check_result = result.type.IsSupportedAndOfType(result.value, self.check_type)
+
+        if result.name is not None and type_check_result[1] is not None:
+            type_overloads[result.name] = type_check_result[1]
+
+        if type_check_result[0]:
+            return result
+
+        return ConstraintExpressionPhrase.EvalResult(False, Boolean(), None)
+
+    # ----------------------------------------------------------------------
+    @Interface.override
+    def ToString(
+        self,
+        args: Dict[str, Any],
+    ) -> str:
+        return "{} is {}".format(
+            self.expression.ToString(args),
+            self.check_type.name,
+        )
--- src/TheLanguage/v1/Parser/Literals/LiteralPhrase.py
+++ src/TheLanguage/v1/Parser/Literals/LiteralPhrase.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------
 # |
-# |  ConstraintExpressionPhrase.py
+# |  LiteralPhrase.py
 # |
 # |  David Brownell <db@DavidBrownell.com>
 # |      2022-04-14 10:04:59
@@ -13,16 +13,15 @@
 # |  http://www.boost.org/LICENSE_1_0.txt.
 # |
 # ----------------------------------------------------------------------
-"""Contains the ConstraintExpressionPhrase object"""
+"""Contains the LiteralPhrase object"""
 
 import os
 
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, List, Optional
 
 from dataclasses import dataclass, InitVar
 
 import CommonEnvironment
-from CommonEnvironment import Interface
 
 from CommonEnvironmentEx.Package import InitRelativeImports
 
@@ -33,13 +32,12 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 
 with InitRelativeImports():
     from ..Phrase import Phrase, Region
-    from ..Common.CompileTimeTypes.CompileTimeType import CompileTimeType
 
 
 # ----------------------------------------------------------------------
 @dataclass(frozen=True, repr=False)
-class ConstraintExpressionPhrase(Phrase, Interface.Interface):
-    """Abstract base class for all constraint expressions"""
+class LiteralPhrase(Phrase):
+    """Abstract base class for all literal types"""
 
     # ----------------------------------------------------------------------
     regions: InitVar[List[Optional[Region]]]
@@ -61,33 +59,4 @@ class ConstraintExpressionPhrase(Phrase, Interface.Interface):
         validate=True,
         **custom_display_funcs: Callable[[Any], Optional[Any]],
     ):
-        super(ConstraintExpressionPhrase, self).__init__(
-            regions,
-            regionless_attributes,
-            validate,
-            expression_type=None,  # type: ignore
-            **custom_display_funcs,
-        )
-
-    # ----------------------------------------------------------------------
-    @dataclass(frozen=True)
-    class EvalResult(object):
-        value: Any
-        type: CompileTimeType
-        name: Optional[str]                 # None if the value is a temporary
-
-    @staticmethod
-    @Interface.abstractmethod
-    def Eval(
-        args: Dict[str, Any],
-        type_overloads: Dict[str, CompileTimeType],
-    ) -> "ConstraintExpressionPhrase.EvalResult":
-        raise Exception("Abstract method")  # pragma: no cover
-
-    # ----------------------------------------------------------------------
-    @staticmethod
-    @Interface.abstractmethod
-    def ToString(
-        args: Dict[str, Any],
-    ) -> str:
-        raise Exception("Abstract method")  # pragma: no cover
+        super(LiteralPhrase, self).__init__(regions, regionless_attributes, validate, **custom_display_funcs)
